3e2ee9dec563e884b494980390fe3cf27b47967e
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-04-19 22:24:54 -0400

    Remove quill-cursors dependence on $nuxt

--- src/stores/main-store.ts
+++ src/stores/main-store.ts
@@ -1,7 +1,14 @@
 import { defineStore } from 'pinia';
 import { AppPage, IPageReference } from 'src/boot/app/page/page';
-import { computed, reactive, toRefs } from 'vue';
+import { computed, reactive, toRefs, watchEffect } from 'vue';
 import { usePageCache } from './page-cache';
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __DEEP_NOTES__: {
+    zoom?: number;
+  };
+}
 
 export const useMainStore = defineStore('main-store', () => {
   // State
@@ -30,6 +37,18 @@ export const useMainStore = defineStore('main-store', () => {
   async function fetchData() {
     console.log('Fetch data here');
   }
+
+  // Zoom watching for quill-cursors
+
+  globalThis.__DEEP_NOTES__ = {};
+
+  watchEffect(() => {
+    if (currentPage.value == null) {
+      return;
+    }
+
+    globalThis.__DEEP_NOTES__.zoom = currentPage.value.camera.react._zoom;
+  });
 
   return {
     ...toRefs(state),
